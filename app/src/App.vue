ad86899775ad3dd24530b623d6bdc440702bdbdd
xxx871 <0kh38c317264.7u@gmail.com>
2024-10-17 14:11:36 +0900

    「3-Coading」の試験提出

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script setup>
+import avatar from './assets/images/avatar.png'
+</script>
 
 <template>
-  <h1>Hello,World</h1>
+  <div class="layout">
+    <aside class="sidebar">
+      <p>サイドメニュー</p>
+      <section class="profile">
+        <div class="avatar">
+          <img :src="avatar" alt="Profile Avatar">
+        </div>
+        <h2>Your Name</h2>
+        <p>役職名</p>
+      </section>
+    </aside>
+    <div class="main-wrapper">
+      <header>
+        <p>ヘッダー</p>
+      </header>
+      <main class="main-content">
+        <p>メイン</p>
+      </main>
+    </div>
+  </div>
+  <footer>
+    <p>フッター</p>
+  </footer>
 </template>
 
 <style scoped></style>
